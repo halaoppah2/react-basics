6888d1fd4dc4ef3ae983444e00d67bb27be81afd
halaoppah2 <enochoppah2@gmail.com>
2025-12-22 18:25:45 +0000

    Use data from the backend to generate the html

--- ecommerce-project/src/Pages/HopePage.jsx
+++ ecommerce-project/src/Pages/HopePage.jsx
@@ -1,14 +1,17 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../Component/Header';
-import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    const [products, setProducts] = useState([]);
 
-    axios.get('http://localhost:3000/api/products')
+    useEffect(() => {
+         axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            console.log(response.data);
+            setProducts(response.data);
             });
+    }, []);
 
     return (
         <>
